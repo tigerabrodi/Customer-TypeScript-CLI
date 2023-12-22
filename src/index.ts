509d18d2895f31439dfb80ddce004143fe938f69
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-22 07:23:33 +0100

    setup main function

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,16 @@ type Customer = {
 }
 
 const customers = dataJSON as Array<Customer>
+
+async function main() {
+  console.log('Hello world!')
+}
+
+main()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
