96e81ec920a7bb39d8f4517a73d5f57acc4dc86f
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-22 07:33:06 +0100

    display menu

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 // - [x] Create a new project
 // - [ ] Display menu with options
 // - [ ] List customer names and emails
-// - [ ] Display customer details -> Input customer ID -> Show details
-// - [ ] Total amount spent by each customer -> Input customer ID -> Show total amount
-// - [ ] Most recent purchase for each customer -> Input customer ID -> Show most recent purchase
+// - [ ] Display customer details -> User inputs customer id, then display "Customer ID: details"
+// - [ ] Total amount spent by each customer: "customer ID: Show total amount"
+// - [ ] Most recent purchase for each customer: "Input customer ID: Show most recent purchase name"
 
 import dataJSON from './data.json'
+
+import { select } from '@inquirer/prompts'
 
 type PurchaseHistory = {
   item: string
@@ -23,8 +25,53 @@ type Customer = {
 
 const customers = dataJSON as Array<Customer>
 
+async function displayMenu() {
+  const answer = await select({
+    message: 'Select a package manager',
+    choices: [
+      {
+        name: 'list',
+        value: 'list',
+        description: 'List customer names and emails',
+      },
+      {
+        name: 'details',
+        value: 'details',
+        description: 'Display customer details',
+      },
+      {
+        name: 'total',
+        value: 'total',
+        description: 'Total amount spent by each customer',
+      },
+      {
+        name: 'recent',
+        value: 'recent',
+        description: 'Most recent purchase for each customer',
+      },
+    ],
+  })
+
+  switch (answer) {
+    case 'list':
+      // listCustomers()
+      break
+    // case 'details':
+    //   displayCustomerDetails()
+    //   break
+    // case 'total':
+    //   totalAmountSpent()
+    //   break
+    // case 'recent':
+    //   mostRecentPurchase()
+    //   break
+    default:
+      break
+  }
+}
+
 async function main() {
-  console.log('Hello world!')
+  await displayMenu()
 }
 
 main()
