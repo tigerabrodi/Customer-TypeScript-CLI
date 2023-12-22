695f425b82d31ff17ced072796dbfece74c5b097
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-22 07:22:09 +0100

    index.ts

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Customer-TypeScript-CLI
+
+TypeScript CLI tool that lets non technical people analyze customer data.
+
+## TODO
+
+- [x] Create a new project
+- [ ] Display menu with options
+- [ ] List customer names and emails
+- [ ] Display customer details -> Input customer ID -> Show details
+- [ ] Total amount spent by each customer -> Input customer ID -> Show total amount
+- [ ] Most recent purchase for each customer -> Input customer ID -> Show most recent purchase
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World!')
+// - [x] Create a new project
+// - [ ] Display menu with options
+// - [ ] List customer names and emails
+// - [ ] Display customer details -> Input customer ID -> Show details
+// - [ ] Total amount spent by each customer -> Input customer ID -> Show total amount
+// - [ ] Most recent purchase for each customer -> Input customer ID -> Show most recent purchase
+
+import dataJSON from './data.json'
+
+type PurchaseHistory = {
+  item: string
+  amount: number
+  purchaseDate: string
+}
+
+type Customer = {
+  id: string
+  name: string
+  email: string
+  registrationDate: string
+  purchaseHistory: Array<PurchaseHistory>
+}
+
+const customers = dataJSON as Array<Customer>
